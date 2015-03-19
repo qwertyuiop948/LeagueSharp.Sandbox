704f30ed2ace97b550639bf786e0a304fae9322e
L33T <worstping.git@gmail.com>
2015-03-20 00:15:05 +0200

    Sandbox: Uploading Bootstrap and DomainProxy.

---
+++ Bootstrap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace LeagueSharp.Sandbox
+{
+    public class Bootstrap
+    {
+        private static readonly List<string> LoadedAssemblies = new List<string>();
+        private static DomainProxy _mainDomainProxy;
+        private static ProcessInputDelegate _processInputDelegate;
+
+        public static void Init(string param)
+        {
+            
+        }
+
+        private static void ProcessInput(ProcessAction action, string args)
+        {
+            if (_mainDomainProxy == null)
+            {
+                _mainDomainProxy = DomainProxy.Create("LeagueSharp\\Sandbox");
+            }
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall, CharSet = CharSet.Unicode)]
+        private delegate void ProcessInputDelegate(ProcessAction action, string args);
+    }
+
+    public enum ProcessAction
+    {
+        Load,
+        Unload
+    }
+}
---
+++ DomainProxy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LeagueSharp.Sandbox
+{
+    public class DomainProxy : MarshalByRefObject
+    {
+        static DomainProxy()
+        {
+            
+        }
+
+        public static DomainProxy Create(string domain)
+        {
+            return null;
+        }
+    }
+}
